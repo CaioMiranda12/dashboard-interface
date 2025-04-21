6c62e6585770e34f5154c4709f1ee273fa9bb577
CaioMiranda12 <caioclm04@gmail.com>
2025-04-21 15:07:41 -0300

    feat: criada interface da tela de cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,30 +1,24 @@
+import { Label } from "@/components/label"
 
 
 export default function Cadastro() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <h1>Faça seu cadastro</h1>
+    <div>
+      <h1 className="text-green-300 font-bold text-xl text-center mt-8 mb-5">{"<FinDash$/>"}</h1>
 
-        <div>
-          <label>Nome</label>
-          <input type="text" placeholder="Digite seu nome..." />
+      <div className="w-10/12 sm:max-w-screen-lg mx-auto">
+        <h1 className="text-white text-3xl mb-4 sm:text-4xl">Criar conta</h1>
+
+        <div className="flex flex-col gap-4">
+          <Label name="Nome" />
+          <Label name="E-mail" />
+          <Label name="Senha" />
+          <Label name="Confirmar senha" />
         </div>
 
-        <div>
-          <label>E-mail</label>
-          <input type="text" placeholder="Digite seu nome..." />
-        </div>
-
-        <div>
-          <label>Senha</label>
-          <input type="text" placeholder="Digite sua senha..." />
-        </div>
-
-        <div>
-          <label>Confirmar senha senha</label>
-          <input type="text" placeholder="Confirme sua senha..." />
-        </div>
+        <button
+          className="bg-emerald-400 w-full h-12 rounded-lg mt-7 cursor-pointer font-semibold hover:bg-emerald-500 transition-all duration-300 sm:h-16 sm:text-xl"
+        >Criar conta</button>
       </div>
     </div>
   )
