d606d60efdec9d48f3bf59ac437dd1d469711234
CaioMiranda12 <caioclm04@gmail.com>
2025-04-21 23:31:41 -0300

    feat: conectando pagina de criar conta com api + axios

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,7 +1,43 @@
+'use client'
 
+import { api } from "@/services/api";
+import { FormEvent, useState } from "react";
+
+interface userData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+async function createUser(data: userData) {
+  const response = await api.post('/users', {
+    name: data.name,
+    email: data.email,
+    password: data.password
+  })
+
+  console.log(response)
+}
 
 
 export default function Cadastro() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    const createdUser = await createUser({
+      name: name,
+      email: email,
+      password: password
+    })
+
+    console.log(createdUser)
+  }
+
   return (
     <div>
       <h1 className="text-green-300 font-bold text-xl text-center mt-8 mb-5">{"<FinDash$/>"}</h1>
@@ -9,26 +45,38 @@ export default function Cadastro() {
       <div className="w-10/12 sm:max-w-screen-lg mx-auto border border-gray-300 px-4 py-8">
         <h1 className="text-white text-3xl mb-4 sm:text-4xl">Criar conta</h1>
 
-        <form className="flex flex-col gap-4">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4">
           <div className="flex flex-col">
             <label className="text-white mb-2">Nome</label>
             <input
               className="border border-white h-10 rounded-lg text-white text-sm px-2 sm:h-14 sm:text-lg"
-              type="text" />
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
           </div>
 
           <div className="flex flex-col">
             <label className="text-white mb-2">E-mail</label>
             <input
               className="border border-white h-10 rounded-lg text-white text-sm px-2 sm:h-14 sm:text-lg"
-              type="text" />
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
 
           <div className="flex flex-col">
             <label className="text-white mb-2">Senha</label>
             <input
               className="border border-white h-10 rounded-lg text-white text-sm px-2 sm:h-14 sm:text-lg"
-              type="password" />
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+
           </div>
 
           <div className="flex flex-col">
@@ -38,11 +86,16 @@ export default function Cadastro() {
               type="password" />
           </div>
 
+          <button
+            type="submit"
+            className="bg-emerald-400 w-full h-12 rounded-lg mt-7 cursor-pointer font-semibold hover:bg-emerald-600 transition-all duration-300 sm:h-16 sm:text-xl"
+          >
+            Criar conta
+          </button>
+
         </form>
 
-        <button
-          className="bg-emerald-400 w-full h-12 rounded-lg mt-7 cursor-pointer font-semibold hover:bg-emerald-600 transition-all duration-300 sm:h-16 sm:text-xl"
-        >Criar conta</button>
+
       </div>
     </div>
   )
