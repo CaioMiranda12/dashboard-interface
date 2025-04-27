44d28f4011f00b485f0cf6a78799685a75ce1d8a
CaioMiranda12 <caioclm04@gmail.com>
2025-04-27 19:27:14 -0300

    feat: criado menu de usuario no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,6 +5,18 @@ import { Button } from "../button";
 import { usePathname } from "next/navigation";
 import { Logo } from "../logo";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { CircleUserRound, Delete, Edit, LogOut, Trash2, UserPen } from "lucide-react";
+
+
 export function Header() {
   const pathname = usePathname();
 
@@ -15,14 +27,43 @@ export function Header() {
       <Logo />
 
       <div className="flex xl:gap-16">
-        <div className="flex gap-4 text-white">
+        <div className="flex gap-4 text-white ml-4">
           <Button>Nova transação</Button>
           <Button>Nova categoria</Button>
         </div>
 
-        <div className="hidden sm:flex items-center xl:gap-3 text-white">
-          <p>Caio César</p>
-          <FaCircleUser color="#fff" size={25} />
+        <div className="flex items-center xl:gap-3 text-white">
+
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <div className="flex gap-3 ml-4">
+                <p className="hidden sm:block">Caio César</p>
+                <FaCircleUser color="#fff" size={25} />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <CircleUserRound />
+                <span>Perfil</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <UserPen />
+                <span>Editar conta</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Trash2 />
+                <span>Deletar conta</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut />
+                <span>Sair da conta</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
