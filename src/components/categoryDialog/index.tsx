842093a2dce2d9831194eb4c82a9ac6299e9730e
CaioMiranda12 <caioclm04@gmail.com>
2025-05-05 12:42:58 -0300

    feat: categoria criada salva na api

--- src/components/categoryDialog/index.tsx
+++ src/components/categoryDialog/index.tsx
@@ -1,16 +1,60 @@
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { api } from "@/services/api"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useForm } from "react-hook-form"
+import { toast } from "react-toastify"
+
+import * as yup from "yup"
+
+interface categoryData {
+  name: string;
+}
 
 export function CategoryDialog() {
+  const schema = yup.object({
+    name: yup.string().required('O nome é obrigatório'),
+  }).required()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = async (data: categoryData) => {
+    try {
+      const { status } = await api.post('/category', {
+        name: data.name,
+      },
+        {
+          validateStatus: () => true
+        })
+
+      if (status === 201 || status === 200) {
+        toast.success('Categoria criada com sucesso')
+      }
+
+      else if (status === 409) {
+        toast.error('Já existe uma categoria com esse nome.')
+      }
+      else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error('Falha no sistema! Tente novamente')
+    }
+  }
+
   return (
-
-
     <Dialog>
       <DialogTrigger className="text-sm text-[#04141C] font-semibold p-2 rounded-lg bg-emerald-400 cursor-pointer hover:bg-emerald-600 transition-all duration-300">
         Nova categoria
@@ -24,24 +68,28 @@ export function CategoryDialog() {
             Crie uma nova categoria para suas transações
           </DialogDescription>
         </DialogHeader>
-        <div className="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col">
             <label className="text-sm">Nome</label>
             <input
               className="bg-black-ofc py-3 px-4"
-              placeholder="Nome da categoria..." />
+              placeholder="Nome da categoria..."
+              {...register('name')}
+            />
+            <p className="text-red-400 font-semibold mt-1">{errors.name?.message}</p>
+
           </div>
 
           <div className="flex justify-end gap-6 mt-4">
-            <button className="border border-primary-ofc px-4 py-2 text-primary-ofc font-semibold rounded-sm cursor-pointer hover:opacity-80 active:opacity-60">
+            <DialogClose className="border border-primary-ofc px-4 py-2 text-primary-ofc font-semibold rounded-sm cursor-pointer hover:opacity-80 active:opacity-60">
               Cancelar
-            </button>
+            </DialogClose>
 
             <button className="border border-primary-ofc bg-primary-ofc px-4 py-2 text-dark-ofc font-semibold rounded-sm cursor-pointer hover:opacity-80 active:opacity-60">
               Cadastrar
             </button>
           </div>
-        </div>
+        </form>
       </DialogContent>
     </Dialog>
   )
---
+++ src/components/transactionDialog/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+export function TransactionDialog() {
+  return (
+
+
+    <Dialog>
+      <DialogTrigger className="text-sm text-[#04141C] font-semibold p-2 rounded-lg bg-emerald-400 cursor-pointer hover:bg-emerald-600 transition-all duration-300">
+        Nova transação
+      </DialogTrigger>
+      <DialogContent className="bg-[#001E2B] text-white">
+        <DialogHeader>
+          <DialogTitle className="text-gray-200 text-2xl flex flex-col gap-2">
+            Nova categoria
+          </DialogTitle>
+          <DialogDescription className="text-gray-300 text-base">
+            Crie uma nova categoria para suas transações
+          </DialogDescription>
+        </DialogHeader>
+        <div className="">
+          <div className="flex flex-col">
+            <label className="text-sm">Nome</label>
+            <input
+              className="bg-black-ofc py-3 px-4"
+              placeholder="Nome da categoria..." />
+          </div>
+
+          <div className="flex justify-end gap-6 mt-4">
+            <button className="border border-primary-ofc px-4 py-2 text-primary-ofc font-semibold rounded-sm cursor-pointer hover:opacity-80 active:opacity-60">
+              Cancelar
+            </button>
+
+            <button className="border border-primary-ofc bg-primary-ofc px-4 py-2 text-dark-ofc font-semibold rounded-sm cursor-pointer hover:opacity-80 active:opacity-60">
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
