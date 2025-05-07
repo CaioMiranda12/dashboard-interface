e62c3d49625a9b719af88b7077d14c8c56929874
CaioMiranda12 <caioclm04@gmail.com>
2025-05-07 13:43:49 -0300

    fix: data correta sendo mostrada nos transactionitems (timezone no convertData)

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,3 +1,8 @@
 export function convertDate(date: string){
-  return new Date(date).toLocaleDateString('pt-BR')
+  return new Date(date).toLocaleDateString('pt-BR', {
+    timeZone: 'UTC',
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  })
 }
