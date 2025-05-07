156a33f7b7c176b683ff8343bfcac7f1f03b2a8a
CaioMiranda12 <caioclm04@gmail.com>
2025-05-07 15:03:14 -0300

    fix: estilização da transactionlist com o fundo correto apos muitos transactions

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       </div>
 
-      <div className="col-span-1 bg-[#001E2B] p-4 h-[85vh]">
+      <div className="col-span-1 bg-[#001E2B] p-4 min-h-[85vh]">
         <h1 className="text-white text-xl font-bold">Transações</h1>
         <p className="text-gray-300">Receitas e Gastos no período</p>
 
