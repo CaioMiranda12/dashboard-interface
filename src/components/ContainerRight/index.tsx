dee2efbd8ed4577aaab7d873d0fd053c894b2ff6
CaioMiranda12 <caioclm04@gmail.com>
2025-05-24 13:49:51 -0300

    fix: container right acompanha o height do container left

--- src/components/ContainerRight/index.tsx
+++ src/components/ContainerRight/index.tsx
@@ -2,7 +2,7 @@ import { TransactionList } from "../transactionList";
 
 export function ContainerRight() {
   return (
-    <div className="col-span-1 bg-[#001E2B] p-4 min-h-[85vh]">
+    <div className="col-span-1 bg-[#001E2B] p-4 min-h-[85vh] h-full">
       <h1 className="text-white text-xl font-bold">Transações</h1>
       <p className="text-gray-300">Receitas e Gastos no período</p>
 
